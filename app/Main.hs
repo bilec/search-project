604aa5c7714378c03fcec0440b98df9011b81957
bilec <martin.bilka826@gmail.com>
2022-05-12 09:21:25 +0200

    added main menu

--- app/Main.hs
+++ app/Main.hs
@@ -4,4 +4,21 @@ import ParseLib
 import PageRankLib
 
 main :: IO ()
-main = pageRank
+main = menu
+
+menu :: IO ()
+menu = do
+    print "Menu:"
+    print "1. Parse collection.jl file"
+    print "2. Calculate pageRank"
+    print "3. Calculate reverse index"
+    print "4. Search"
+
+    userInput <- getLine
+    case userInput of 
+        "1" -> parse
+        "2" -> pageRank
+        "3" -> print "3 selected"
+        "4" -> print "4 selected"
+        otherwise -> print "wrong option, try again"
+    menu
