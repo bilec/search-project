d94a0c4956619364831ca44d707c27aa8b780a96
bilec <martin.bilka826@gmail.com>
2022-05-20 21:04:02 +0200

    slightly faster inverted index calculation

--- src/InvertedIndexLib.hs
+++ src/InvertedIndexLib.hs
@@ -11,8 +11,9 @@ import qualified Data.ByteString.Lazy as B
 import qualified Data.ByteString.Lazy.Char8 as BB
 
 import GHC.IO.Encoding (setLocaleEncoding, latin1)
-import Data.List (nub)
-import Data.Map (Map, empty)
+import Data.List (nub, foldl1', foldr1)
+import Data.Map (unionWith, fromList, size, Map, empty, filterWithKey, filter, elems, mapWithKey, map, keys, toList, elemAt, singleton, findIndex, elemAt)
+import Data.Set (fromList, unions, union, toAscList, Set, singleton, empty, size, map, toList)
 import Data.Maybe (isNothing, fromJust)
 import Data.Typeable
 import GHC.Generics
@@ -29,7 +30,7 @@ instance FromJSON WebPageInfoJson
 
 data InvertedIndex =
   InvertedIndex { 
-    index :: [(T.Text,[T.Text])]
+    index :: Map T.Text (Set T.Text)
   } deriving (Show, Generic)
 
 instance FromJSON InvertedIndex
@@ -46,29 +47,35 @@ getWords :: WebPageInfoJson -> [(T.Text, Int)]
 getWords json = textContentWords json
 
 stripOccurence :: [[(a, b)]] -> [[a]]
-stripOccurence = map (map fst)
+stripOccurence = Prelude.map (Prelude.map fst)
 
 flattenWords :: [[(a, b)]] -> [a]
-flattenWords = concatMap (map fst)
+flattenWords = concatMap (Prelude.map fst)
 
 urlsForWord :: T.Text -> [(T.Text, [T.Text])] -> [T.Text]
 urlsForWord word urlWordsTupleList = 
-  let containsWord = filter (\x -> elem word (snd x)) urlWordsTupleList
-  in map (\x -> (fst x)) containsWord
+  let containsWord = Prelude.filter (\x -> elem word (snd x)) urlWordsTupleList
+  in Prelude.map (\x -> (fst x)) containsWord
+
+processOneIncoming :: (T.Text, [T.Text]) -> Map T.Text (Set T.Text)
+processOneIncoming (link, wordsForLink) =
+  let linksForWord = Prelude.map (\x -> Data.Map.singleton (x) (Data.Set.singleton link)) wordsForLink
+  in if (null linksForWord)
+    then 
+      Data.Map.singleton link Data.Set.empty
+    else 
+      Data.Map.unionWith (Data.Set.union) (Data.Map.singleton link Data.Set.empty) (foldr1 (unionWith (Data.Set.union)) linksForWord)
 
 createInvertedIndex :: IO ()
 createInvertedIndex = do 
   jsonCollectionFile <- BB.readFile "webPageInfo.txt"
-  let jsonList = filter (not . BB.null) (BB.lines jsonCollectionFile)
-  let jsonDecodedListMaybe = map (decodeWebPageInfoJson) jsonList
-  let jsonDecodedList = map (fromJust) (filter (not . isNothing) jsonDecodedListMaybe)
-  let links = map (getLinks) jsonDecodedList
-  let words = map (getWords) jsonDecodedList
-  let wordsClean = (stripOccurence) words
-  let urlWordsTuple = zip links wordsClean
-  let wordsList = nub ((flattenWords) words)
-  let wordsWithUrl = map (\x -> (x,(urlsForWord x urlWordsTuple))) wordsList 
-  let invertedIndex = InvertedIndex wordsWithUrl
+  let jsonList = Prelude.filter (not . BB.null) (BB.lines jsonCollectionFile)
+  let jsonDecodedListMaybe = Prelude.map (decodeWebPageInfoJson) jsonList
+  let jsonDecodedList = Prelude.map (fromJust) (Prelude.filter (not . isNothing) jsonDecodedListMaybe)
+
+  let preparedJsonList = Prelude.map (\x -> ((urlLink x), Prelude.map (\y -> fst y) (textContentWords x))) jsonDecodedList
+  let linksForWord = foldr1 (unionWith (Data.Set.union)) (Prelude.map (processOneIncoming) preparedJsonList)
+  let invertedIndex = InvertedIndex linksForWord
   let invertedIndexEncoded = encode invertedIndex
   setLocaleEncoding latin1
 
